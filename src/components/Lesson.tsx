550f959a3c1c2d89ca9f8ba203fe7e73aeb27da1
Felipe Ribeiro <piporibeiro@gmail.com>
2022-06-26 21:48:33 -0300

    feat: adjust when Lesson is not active

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -24,69 +24,76 @@ export const Lesson = (props: LessonProps) => {
 
   const isActiveLesson = slug === props.slug
   return (
-    <Link to={`/event/lesson/${props.slug}`} className={'group relative'}>
-      <span className={'text-gray-300'}>
-        {availableDateFormat[0].toUpperCase() +
-          availableDateFormat.substring(1)}
-      </span>
-
-      {isActiveLesson ? (
-        <span
-          className={classNames(
-            'bg-green-500 w-4 h-4 rotate-45 absolute top-20 right-72 ',
-          )}
-        />
-      ) : null}
-      <div
-        className={classNames(
-          'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ',
-          {
-            'bg-green-500 ': isActiveLesson,
-          },
-        )}
+    <div className={`${!isLessonAvailable ? 'cursor-not-allowed' : ''} `}>
+      <Link
+        to={`/event/lesson/${props.slug}`}
+        className={classNames('group relative ', {
+          'pointer-events-none ': !isLessonAvailable,
+        })}
       >
-        <header className={'flex items-center justify-between '}>
-          {isLessonAvailable ? (
+        <span className={'text-gray-300'}>
+          {availableDateFormat[0].toUpperCase() +
+            availableDateFormat.substring(1)}
+        </span>
+
+        {isActiveLesson ? (
+          <span
+            className={classNames(
+              'bg-green-500 w-4 h-4 rotate-45 absolute top-20 right-72 ',
+            )}
+          />
+        ) : null}
+        <div
+          className={classNames(
+            'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ',
+            {
+              'bg-green-500 ': isActiveLesson,
+            },
+          )}
+        >
+          <header className={'flex items-center justify-between '}>
+            {isLessonAvailable ? (
+              <span
+                className={classNames(
+                  'text-sm text-blue-500 font-medium flex items-center gap-2',
+                  {
+                    'text-gray-100': isActiveLesson,
+                  },
+                )}
+              >
+                <CheckCircle size={20} />
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span
+                className={
+                  'text-sm text-orange-500 font-medium flex items-center gap-2'
+                }
+              >
+                <Lock size={20} />
+                Em Breve
+              </span>
+            )}
             <span
               className={classNames(
-                'text-sm text-blue-500 font-medium flex items-center gap-2',
+                'text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold',
                 {
-                  'text-gray-100': isActiveLesson,
+                  'border-gray-100': isActiveLesson,
                 },
               )}
             >
-              <CheckCircle size={20} />
-              Conteúdo liberado
+              {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
             </span>
-          ) : (
-            <span
-              className={
-                'text-sm text-orange-500 font-medium flex items-center gap-2'
-              }
-            >
-              <Lock size={20} />
-              Em Breve
-            </span>
-          )}
-          <span
-            className={classNames(
-              'text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold',
-              {
-                'border-gray-100': isActiveLesson,
-              },
-            )}
+          </header>
+          <strong
+            className={classNames('text-gray-200 mt-5 block', {
+              'text-gray-100': isActiveLesson,
+            })}
           >
-            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-          </span>
-        </header>
-        <strong
-          className={classNames('text-gray-200 mt-5 block', {
-            'text-gray-100': isActiveLesson,
-          })}
-        >
-          {props.title}
-        </strong>
-      </div>
-    </Link>
+            {props.title}
+          </strong>
+        </div>
+      </Link>
+    </div>
   )
 }
